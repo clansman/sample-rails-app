7834972bba55b3681b39c827978524807ed98d1b
Zulfat <zulfatkin@gmail.com>
2014-08-05 18:42:37 +0400

    finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,42 +4,26 @@ describe 'Static Page' do
   subject { page }
   describe 'Home page' do
     before { visit root_path}
-
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
   describe 'Help page' do
-    it "should have content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-    it "should have title with 'Help'" do
-      visit help_path
-      expect(page).to have_title('Help')
-    end
+    before {visit help_path}
+    it {should have_content('Help')}
+    it {should have_title(full_title('Help'))}
   end
 
   describe 'About page' do
-    it "should have content 'About page'" do
-      visit about_path
-      expect(page).to have_content('About')
-    end
-    it "should have title with 'About'" do
-      visit about_path
-      expect(page).to have_title('About')
-    end
+    before {visit about_path}
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
   end
 
   describe 'Contact page' do
-    it "should have contact 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
-    it "should have title with 'Contact'" do
-      visit contact_path
-      expect(page).to have_title('Contact')
-    end
+    before {visit contact_path}
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
